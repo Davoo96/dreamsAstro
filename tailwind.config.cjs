de594fe7449b3b3638b62a31537b6b505869be1b
Davoo96 <davizi96@hotmail.com>
2023-01-26 21:16:26 -0300

    feat(projects): adding project component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,10 +17,16 @@ module.exports = {
       },
       backgroundImage: {
         "header-image":
-          "linear-gradient(#000, transparent), url('/src/assets/pictures/mobile-00.png')",
+          "linear-gradient(#1f2224, transparent), url('/src/assets/pictures/mobile-00.png')",
         "header-desktop-image":
-          "linear-gradient(#000, transparent), url('/src/assets/pictures/JanduiCasas5.png')",
-        "header-gradient": "linear-gradient(#000,transparent)"
+          "linear-gradient(#1f2224, transparent), url('/src/assets/pictures/JanduiCasas5.png')",
+        "header-gradient": "linear-gradient(#1f2224, transparent)",
+        "novo-residence": "linear-gradient(#1f2224, 80%, transparent), url('/src/assets/pictures/mobile-01.png')",
+        "novo-residence-desktop": "linear-gradient(#1f2224, transparent), url('/src/assets/pictures/pht-03.png')",
+        "jabuticabeira": "linear-gradient(#1f2224, 80%, transparent), url('/src/assets/pictures/mobile-02.png')",
+        "jabuticabeira-desktop": "linear-gradient(#1f2224, transparent), url('/src/assets/pictures/pht-04.png')",
+        "view": "linear-gradient(#1f2224, 80%, transparent), url('/src/assets/pictures/mobile-03.png')",
+        "view-desktop": "linear-gradient(#1f2224, transparent), url('/src/assets/pictures/pht-36.png')"
       },
       backgroundSize: {
         100: "100% 100%"
